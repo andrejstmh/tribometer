8885edf52b2de5ffe989bed324cdd1d907a23c25
Andrejs Timuhins <andrejstmh@gmail.com>
2019-06-11 20:52:57 +0300

    calibration table

--- webapp/src/app/components/calibr-curve/calibr-curve.component.ts
+++ webapp/src/app/components/calibr-curve/calibr-curve.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { SygnalsService, CalibrationCurve, trTotalState } from './../../services/sygnals.service';
 import { Observable, BehaviorSubject } from 'rxjs';
 
+class CurveRow {
+	constructor(public x: number = 0, public y: number = 0, public Nr: number = null) { }
+}
 
 @Component({
   selector: 'app-calibr-curve',
@@ -16,7 +19,12 @@ export class CalibrCurveComponent implements OnInit {
     { name: "Load calibration curve", path: "clbr_load", title: ["Voltage, V", "Force, N"] }
     //{ name: "RPM calibration curve", path: "clbr_rpm", title: ["Voltage, V", "RPM"] }
   ]
-  CcurvData: CalibrationCurve = null;
+	data$: BehaviorSubject<CurveRow[]> = new BehaviorSubject<CurveRow[]>([]);
+	editCurve: boolean = false;
+	selectedRow: CurveRow = new CurveRow();
+	prevData: CurveRow[] = [];
+
+	CcurvData: CalibrationCurve = null;
   status$: BehaviorSubject<trTotalState> = null;
   constructor(private sygnalServ: SygnalsService) {
     this.status$ = this.sygnalServ.totalstate$;
@@ -93,10 +101,13 @@ export class CalibrCurveComponent implements OnInit {
       this.sygnalServ.GetCalibrationCurve(curve.path).subscribe(cc => {
         console.log("GetCalibrationCurve");
         this.CcurvData = cc;
-        this.CurveTable = [];
-        for (var i = 0; i < cc.x.length; i++) {
-          this.CurveTable.push({ x: cc.x[i], y: cc.y[i], select: true });
-        }
+		this.CurveTable = [];
+		let res: CurveRow[] = [];
+		for (var i = 0; i < cc.x.length; i++) {
+			res.push(new CurveRow(cc.x[i], cc.y[i],i));
+			this.CurveTable.push({ x: cc.x[i], y: cc.y[i], select: true });
+		}
+		this.data$.next(res);
         this.drawChart(cc);
       });
     }
@@ -112,5 +123,145 @@ export class CalibrCurveComponent implements OnInit {
   }
   onRestore() {
     //this.sygnalServ.EditCalibrationCurve(this.selectedCurve.path,)
-  }
+	}
+
+
+	
+	CreateNewItem(Nr?: number) {
+		if (Nr === undefined) {
+			return new CurveRow(0, 0, null);
+		} else {
+			return new CurveRow(0, 0, Nr);
+		}
+	}
+
+	EditClic() {
+		this.editCurve = !this.editCurve;
+		if (this.editCurve) {
+			let res = this.data$.value;
+			res.forEach((r, i) => { r.Nr = i; });
+			this.prevData = []
+			res.forEach(r => { this.prevData.push(new CurveRow(r.x, r.y, r.Nr));})
+			this.selectedRow = res[0];
+		} else {
+			this.CancelEdit();
+		}
+	}
+
+	onSelectRow(r: any) {
+		if (this.editCurve) {
+			this.selectedRow = r;
+		}
+	}
+	DeleteRow() {
+		if (this.selectedRow.Nr != null) {
+			let res = this.data$.value;
+			if (res.length > 1) {
+				//res.bazesCena.content = res.bazesCena.content.filter(it => it["Nr"] !== this.selectedRow.Nr)
+				res.splice(this.selectedRow.Nr, 1);
+				res.forEach((r, i) => { r.Nr = i; });
+				this.selectedRow = this.CreateNewItem(null);//{ name: null, val: null, Nr: null };
+				this.data$.next(res);
+			}
+		}
+	}
+	InsertItem(before: boolean) {
+		let nr: number = this.selectedRow.Nr;
+		let res = this.data$.value;
+		let newItem = this.CreateNewItem();
+		if (nr != null) {
+			if (before) {
+				res.splice(nr, 0, newItem);
+			} else {
+				res.splice(nr + 1, 0, newItem);
+				nr = nr + 1;
+			}
+		} else {
+			if (before) {
+				res.unshift(newItem);
+				nr = 0;
+			} else {
+				res.push(newItem);
+				nr = res.length - 1;
+			}
+
+		}
+		res.forEach((r, i) => { r.Nr = i; });
+		this.selectedRow = res[nr];
+		this.data$.next(res);
+
+	}
+
+	InserRowBefore() {
+		this.InsertItem(true);
+	}
+
+	InserRowAfter() {
+		this.InsertItem(false);
+	}
+
+	MoveItem(up: boolean) {
+		let nr: number = this.selectedRow.Nr;
+		let res = this.data$.value;
+		let moved: boolean = false;
+		if (nr != null) {
+			if (up) {
+				if (nr > 0) {
+					let it = res[nr];
+					res[nr] = res[nr - 1];
+					nr = nr - 1;
+					res[nr] = it;
+					moved = true;
+				}
+			} else {
+				if (nr < res.length - 1) {
+					let it = res[nr];
+					res[nr] = res[nr + 1];
+					nr = nr + 1;
+					res[nr] = it;
+					moved = true;
+				}
+			}
+			if (moved) {
+				res.forEach((r, i) => { r.Nr = i; });
+				this.selectedRow = res[nr];
+				this.data$.next(res);
+			}
+		}
+	}
+
+	MoveUp() {
+		this.MoveItem(true);
+	}
+
+	MoveDown() {
+		this.MoveItem(false);
+	}
+
+	SaveData() {
+		let res = this.data$.value;
+		
+		//let s = res.CMS.ToString();
+		//this.sygnalServ.EditCalibrationCurve(this.selectedCurve.path).subscribe();
+
+		//this.cmsService.AizpilditCMS(res.CMS.id, s).subscribe(
+		//	x => {
+		//		//console.log(x);
+		//		this.selectedRow = this.CreateNewItem(null);
+		//		this.prevData = s;
+		//	},
+		//	err => {
+		//		this.dataState$.next(DataState.Error);
+		//		this.CancelEdit();
+		//	}
+		//);
+	}
+
+	CancelEdit() {
+		this.editCurve = false;
+		this.selectedRow = this.CreateNewItem(null);
+		let res: CurveRow[] =[];
+		this.prevData.forEach(r => { res.push(new CurveRow(r.x, r.y, r.Nr)); });
+		this.data$.next(res);
+	}
 }
