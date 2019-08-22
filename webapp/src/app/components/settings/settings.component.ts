fb19bc056982e2efd79aca2018b7bb18d17a74bb
Andrejs Timuhins <andrejstmh@gmail.com>
2019-08-22 19:49:01 +0300

    settings form updated

--- webapp/src/app/components/settings/settings.component.ts
+++ webapp/src/app/components/settings/settings.component.ts
@@ -3,11 +3,37 @@ import { Router } from "@angular/router"
 import { Observable, of, Subject, BehaviorSubject, forkJoin, Subscription } from 'rxjs';
 import { catchError, map, tap, debounceTime, distinctUntilChanged, switchMap, startWith } from 'rxjs/operators';
 
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import {
+    FormGroup, FormControl, FormGroupDirective, NgForm, Validators, AbstractControl,
+    ValidationErrors,ValidatorFn, AsyncValidatorFn
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { trTotalState, trState, trSettings, ObjHelper, trProgram } from './../../models/message.model';
 import { SignalsService } from './../../services/signals.service';
 import { ChartService, LineChartSettings } from './../../services/chart.service';
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class settingsErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        //const isSubmitted = form && form.submitted;
+        //return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+        return !!(control && control.invalid);
+    }
+}
+
+export function outputFileExistsValidator(signalsService: SignalsService): AsyncValidatorFn {
+    return (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
+        return signalsService.GetOutputFileExists(control.value).pipe(
+            debounceTime(300), distinctUntilChanged(),
+            map(
+                fe => {
+                    console.log("outputFileExistsValidator "+fe)
+                    return (fe == "1") ? { "outputFileExists": true } : null;
+            }
+            )
+        );
+    };
+};
 
 @Component({
     selector: 'app-settings',
@@ -22,14 +48,38 @@ export class SettingsComponent implements OnInit, OnDestroy {
     //totState: trTotalState = new trTotalState(null, null);
     OnNewSetting: Subscription = null;
     OnNewStatus: Subscription = null;
+    settingsForm: FormGroup = null;
+    matcher = new settingsErrorStateMatcher();
     constructor(
         private signalsService: SignalsService,
         private router: Router
-    ) { }
+    ) {
+        this.settingsForm = new FormGroup({
+            userControl: new FormControl('', [Validators.required]),
+            bearingDescrControl: new FormControl('', [Validators.required]),
+            outputFileNameControl: new FormControl('', [Validators.required], [outputFileExistsValidator(this.signalsService)]),
+            recIntervalControl: new FormControl(1, [Validators.required, Validators.max(600), Validators.min(1)]),
+            manualModeControle: new FormControl(true, []),
+            durationControl: new FormControl(10, [Validators.required, Validators.min(1), Validators.max(3000)]),
+            loadControl: new FormControl(200, [Validators.required, Validators.min(20), Validators.max(1200)]),
+            RPMControl: new FormControl(300, [Validators.required, Validators.min(100), Validators.max(1200)]),
+            frictThreshControl: new FormControl(100, [Validators.required, Validators.min(5), Validators.max(120)]),
+            tempThreshControl: new FormControl(100, [Validators.required, Validators.min(10), Validators.max(120)]),
+        });
+    }
+    
+    get userControl() { return this.settingsForm.get('userControl'); }
+    get bearingDescrControl() { return this.settingsForm.get('bearingDescrControl'); }
+    get outputFileNameControl() { return this.settingsForm.get('outputFileNameControl'); }
+    get recIntervalControl() { return this.settingsForm.get('recIntervalControl'); }
+    get manualModeControle() { return this.settingsForm.get('manualModeControle'); }
 
-    userControl = new FormControl('', [Validators.required]);
-    bearingDescrControl = new FormControl('', [Validators.required]);
-    outputFileNameControl = new FormControl('', [Validators.required]);
+    get durationControl() { return this.settingsForm.get('durationControl'); }
+    get loadControl() { return this.settingsForm.get('loadControl'); }
+    get RPMControl() { return this.settingsForm.get('RPMControl'); }
+    get frictThreshControl() { return this.settingsForm.get('frictThreshControl'); }
+    get tempThreshControl() { return this.settingsForm.get('tempThreshControl'); }
+
     filteredOptions: Observable<string[]>;
 
     private _filter(value: string): string[] {
@@ -37,13 +87,35 @@ export class SettingsComponent implements OnInit, OnDestroy {
         return this.users$.value.filter(option => option.toLowerCase().includes(filterValue));
     }
 
+    get programmList(): trProgram[] {
+        if (this.lSettings) {
+            if (this.lSettings.manual_mode) {
+                return this.lSettings.program;
+            } else {
+                return this.lSettings.program.slice(0, 1);
+            }
+        } else { return [] };
+    }
     ngOnInit() {
-        this.userControl.setValue("");
         this.filteredOptions = this.userControl.valueChanges
             .pipe(
                 startWith(''),
                 map(value => this._filter(value))
             );
+
+        this.bearingDescrControl.valueChanges.subscribe(resOk => { this.lSettings.bearing = resOk });
+        //GetOutputFileExists
+        this.outputFileNameControl.valueChanges.pipe(debounceTime(100), distinctUntilChanged()).
+            subscribe(resOk => { this.lSettings.output_file = resOk });
+        this.recIntervalControl.valueChanges.subscribe(resOk => { this.lSettings.recording_cycle = resOk });
+        this.manualModeControle.valueChanges.subscribe(resOk => { this.lSettings.manual_mode = resOk });
+
+        this.durationControl.valueChanges.subscribe(resOk => { if (this.selectedRow) { this.selectedRow.duration = resOk; } });
+        this.loadControl.valueChanges.subscribe(resOk => { if (this.selectedRow) { this.selectedRow.load = resOk; } });
+        this.RPMControl.valueChanges.subscribe(resOk => { if (this.selectedRow) { this.selectedRow.RPM = resOk; } });
+        this.frictThreshControl.valueChanges.subscribe(resOk => { if (this.selectedRow) { this.selectedRow.Fmax = resOk; }});
+        this.tempThreshControl.valueChanges.subscribe(resOk => { if (this.selectedRow) { this.selectedRow.Tmax = resOk; }});
+
 
         this.fileExists$ = this.outputFileName$.pipe(
             debounceTime(300),
@@ -75,8 +147,18 @@ export class SettingsComponent implements OnInit, OnDestroy {
                     this.lSettings = ObjHelper.DeepCopyOfState(resOk);
                     this.lSettings.recording_cycle = Math.round(resOk.recording_cycle * resOk.listening_interval / 1000.0);
                     this.lSettings.manual_mode = !resOk.manual_mode;
-                    this.bearingDescrControl.setValue(this.lSettings.bearing)
-                    this.outputFileNameControl.setValue(this.lSettings.output_file)
+
+                    this.bearingDescrControl.setValue(this.lSettings.bearing);
+                    this.outputFileNameControl.setValue(this.lSettings.output_file);
+                    this.recIntervalControl.setValue(this.lSettings.recording_cycle);
+                    this.manualModeControle.setValue(this.lSettings.manual_mode);
+
+                    this.durationControl.setValue(this.lSettings.program[0].duration);
+                    this.loadControl.setValue(this.lSettings.program[0].load);
+                    this.RPMControl.setValue(this.lSettings.program[0].RPM);
+                    this.frictThreshControl.setValue(this.lSettings.program[0].Fmax);
+                    this.tempThreshControl.setValue(this.lSettings.program[0].Tmax);
+
                 } else {
                     this.lSettings = null;
                 }
@@ -94,7 +176,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
             resOk => {
                 this.users$.next(resOk);
                 if ((this.users$.value) && (this.users$.value.length > 0)) {
-                    this.userControl.setValue(this.users$.value[0]);
+                    this.settingsForm.get("userControl").setValue(this.users$.value[0]);
                 }
             }
         );
@@ -112,6 +194,11 @@ export class SettingsComponent implements OnInit, OnDestroy {
 
     onSelectRow(r: any) {
         this.selectedRow = r;
+        this.durationControl.setValue(r.duration);
+        this.loadControl.setValue(r.load);
+        this.RPMControl.setValue(r.RPM);
+        this.frictThreshControl.setValue(r.Fmax);
+        this.tempThreshControl.setValue(r.Tmax);
     }
 
     DeleteRow() {
